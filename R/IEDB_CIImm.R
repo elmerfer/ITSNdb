6ce8b7058085575ee543d93dc37458f5260b68e9
elmerfer <elmerfer@gmail.com>
2022-10-06 18:24:11 -0300

    CIImm iedb implemented

--- R/IEDB_CIImm.R
+++ R/IEDB_CIImm.R
@@ -11,7 +11,7 @@ CIImm <- function(pep){
   
   pep <- seqinr::s2c(pep)
   if(length(pep) !=9 ){
-    stop("only 9-mer peptides ")
+    return(NA)
   }
   data("CIImm")
   E <- as.numeric(aas[pep,2])
@@ -19,12 +19,31 @@ CIImm <- function(pep){
   s <- sum(E*I)
   return(s)
 }
-pep<-"SLFNTVATL"
-c(CIImm("FIAGLIAIV"),
-CIImm("VFIAGLIAI"),
-CIImm("IAGLIAIVV"))
-FIAGLIAIV
-KAVYNFATC
-RLNEVAKNL
-LITGRLQSL
-FQPQNGQFI
+
+#' RunClassIImmunogenicity
+#' It apply the Class I Immunogenicity (CIImm) score (IEDB) to cohort with a list of peptide-HLA pairs.
+#' Up to now only 9-mer peptides
+#' @param pepFile the full path to the peptide-HLA pairs
+#' @export
+#' @return a data frame with the uploaded peptide_HLA pairs with extra columns added by the CIImm output
+#' @usage 
+#' \dontrun{
+#' df.to.test <- data.frame(Sample = c("Subject1","Subject1","Subject2"), Neoantigen=ITSNdb$Neoantigen[1:3],HLA = ITSNdb$HLA[1:3])
+#' write.csv(df.to.test,file="MyPatientsNeoantigenList.csv",quote=F, row.names = F)
+#' run predictions 
+#' Cohort_results <- RunClassIImmunogenicity(pepsFile = "MyPatientsNeoantigenList.csv")
+#' }
+#' 
+
+
+RunClassIImmunogenicity <- function(pepFile){
+  peps <- read.csv(pepFile,h=T)
+  if(all(c("Sample","Neoantigen","HLA") %in% colnames(peps) )==FALSE){
+    stop("colnames error: it shuould contain at least the following columns Sample,Neoantigen,HLA")
+  }
+  
+  peps$CIImm <- unlist(lapply(as.character(peps$Neoantigen), CIImm))
+  return(peps)
+  
+}
+
