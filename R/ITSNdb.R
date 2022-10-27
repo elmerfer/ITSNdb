81fe7e15fed5bd87a0122d417e764019e80c878d
gnibeyro <gnibeyro@gmail.com>
2022-10-27 11:12:17 -0300

    validation and TNB datasets

--- R/ITSNdb.R
+++ R/ITSNdb.R
@@ -21,7 +21,7 @@
 #' @docType data
 #' @keywords datasets
 #' @name ITSNdb
-#' @usage load(ITSNdb)
+#' @usage data(ITSNdb)
 #' @format A data frame with 199 rows (neoantigens) and 11 variables (neoantigens info)
 NULL
 
@@ -36,6 +36,44 @@ NULL
 #' @docType data
 #' @keywords datasets
 #' @name MHC_allele_names
-#' @usage load(MHC_allele_names)
+#' @usage data(MHC_allele_names)
 #' @format A data frame with 1 column and 3584 rows
+NULL
+
+#' TNB_dataset
+#' 
+#' Clinical ICB cohort 
+#' containing putative neoantigens from patients of 6 different clinical cohorts, associated with immunotherapy response 
+#' 
+#' \itemize{
+#' \item Sample: Patient where the neoantigen was reported
+#' \item Neoantigen: Aminoacid sequence of the mutated putative neoantigen
+#' \item HLA: HLA associated to the putative neoantigen
+#' \item Response: Clinical response to ICB
+#' \item Cohort: Cohort where the patient was reported
+#' }
+#' @docType data
+#' @keywords datasets
+#' @name TNB_dataset
+#' @usage data(TNB_dataset)
+#' @format A data frame with 99489 rows (neoantigens) and 5 variables (neoantigens metadata)
+NULL
+
+#' Val_dataset
+#' 
+#' Validation dataset, real scenario simulation 
+#' containing 113 non-immunogenic neopeptides and 7 immunogenic neoantigens from genomic rearrengements different than SNVs 
+#' 
+#' \itemize{
+#' \item Sample: Negative or positive immunogenicity
+#' \item Neoantigen: Aminoacid sequence of the mutated neoantigen
+#' \item HLA: HLA associated to the neoantigen
+#' \item Author: Name of the first author in reference manuscript
+#' \item Origin: Genomic event that originated the neoantigen
+#' }
+#' @docType data
+#' @keywords datasets
+#' @name Val_dataset
+#' @usage data(TNB_dataset)
+#' @format A data frame with 120 rows (neoantigens) and 5 variables (neoantigens metadata)
 NULL
